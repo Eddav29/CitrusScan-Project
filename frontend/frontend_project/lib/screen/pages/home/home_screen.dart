63b8fffb652707c98282c1f24e5c0e9b98fd9cb3
fanesabhirawaning <fanesahbi22@gmail.com>
2024-12-06 09:26:52 +0700

    Update : Home Screen

--- frontend/frontend_project/lib/screen/pages/home/home_screen.dart
+++ frontend/frontend_project/lib/screen/pages/home/home_screen.dart
@@ -3,11 +3,9 @@ import 'package:image_picker/image_picker.dart';
 import '../../common/widgets/navigation_bar.dart';
 import 'tips_widget.dart';
 import '../scan/recent_scan_widget.dart';
-import 'data_jeruk_widget.dart';
 import '../../common/widgets/app_bar.dart';
 import '../history/scan_history_screen.dart';
 import '../search/search_disease.dart';
-import '../scan/scan_result_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -17,56 +15,7 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   Color appBarColor = Colors.transparent;
 
-  // Data dummy untuk riwayat jeruk
-  final List<Map<String, String>> jerukData = [
-    {
-      "namaJeruk": "Jeruk Manis",
-      "tanggalScan": "2 Okt 2024",
-      "imagePath": 'assets/images/jeruk1.png',
-    },
-    {
-      "namaJeruk": "Jeruk Nipis",
-      "tanggalScan": "10 Okt 2024",
-      "imagePath": 'assets/images/jeruknipis.jpeg',
-    },
-    {
-      "namaJeruk": "Jeruk Bali",
-      "tanggalScan": "12 Okt 2024",
-      "imagePath": 'assets/images/jeruk1.png',
-    },
-    {
-      "namaJeruk": "Jeruk Keprok",
-      "tanggalScan": "15 Okt 2024",
-      "imagePath": 'assets/images/jeruknipis.jpeg',
-    },
-  ];
 
-  List<Widget> buildJerukRows() {
-    List<Widget> rows = [];
-    for (int i = 0; i < jerukData.length; i += 2) {
-      DataJerukWidget leftCard = DataJerukWidget(
-        namaJeruk: jerukData[i]["namaJeruk"]!,
-        tanggalScan: jerukData[i]["tanggalScan"]!,
-        imagePath: jerukData[i]["imagePath"]!,
-      );
-
-      DataJerukWidget rightCard = (i + 1 < jerukData.length)
-          ? DataJerukWidget(
-              namaJeruk: jerukData[i + 1]["namaJeruk"]!,
-              tanggalScan: jerukData[i + 1]["tanggalScan"]!,
-              imagePath: jerukData[i + 1]["imagePath"]!,
-            )
-          : DataJerukWidget(
-              namaJeruk: "",
-              tanggalScan: "",
-              imagePath: "",
-            );
-
-      rows.add(JerukRowWidget(leftCard: leftCard, rightCard: rightCard));
-      rows.add(SizedBox(height: 10));
-    }
-    return rows;
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -144,7 +93,7 @@ class _HomeScreenState extends State<HomeScreen> {
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Text(
-                        "Riwayat Deteksi Terakhir",
+                        "Riwayat Scan Terakhir",
                         style: TextStyle(
                           fontSize: 20,
                           fontWeight: FontWeight.bold,
@@ -173,46 +122,35 @@ class _HomeScreenState extends State<HomeScreen> {
                     ],
                   ),
                   SizedBox(height: 10),
-                  // Menampilkan riwayat scan dengan _buildScanHistoryCard
-                  _buildScanHistoryCard(
-                    context,
-                    imagePath: 'assets/images/jeruk1.png',
-                    deteksi: 'Jeruk Manis',
-                    tanggal: '2 Okt 2024',
-                    saran: 'Cek lebih lanjut untuk kualitas jeruk.',
-                    isSelected: false,
-                  ),
+                  // Menampilkan riwayat scan
+                  RecentScanWidget(),
                   SizedBox(height: 20),
-                  // Data jeruk saya
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        "Data Jeruk Saya",
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          fontFamily: 'Gilroy',
-                        ),
-                      ),
-                      Container(
-                        decoration: BoxDecoration(
-                          color: Color(0xFF215C3C),
-                          shape: BoxShape.circle,
-                        ),
-                        child: IconButton(
-                          icon: Icon(Icons.arrow_forward, color: Colors.white),
-                          onPressed: () {
-                            print("Lihat Selengkapnya Data Jeruk tapped");
-                          },
-                        ),
-                      ),
-                    ],
+                   // Bagian kategori penyakit
+                  Text(
+                    "Kategori Penyakit",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Gilroy',
+                    ),
                   ),
                   SizedBox(height: 10),
-                  // Menampilkan semua data jeruk
-                  Column(
-                    children: buildJerukRows(),
+                  GridView.count(
+                    shrinkWrap: true,
+                    crossAxisCount: 2, // 2 kolom per baris
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                    childAspectRatio: 3 / 4, // Proporsi untuk mendekati desain
+                    physics: NeverScrollableScrollPhysics(),
+                    children: [
+                      _buildDiseaseCard(
+                          'Black Spot', 'assets/images/black-spot.jpg'),
+                      _buildDiseaseCard(
+                          'Melanose', 'assets/images/melanose.jpeg'),
+                      _buildDiseaseCard('Canker', 'assets/images/canker.jpg'),
+                      _buildDiseaseCard(
+                          'Greening', 'assets/images/greening.jpeg'),
+                    ],
                   ),
                 ],
               ),
@@ -223,74 +161,78 @@ class _HomeScreenState extends State<HomeScreen> {
       bottomNavigationBar: CustomNavigationBar(),
     );
   }
-}
 
-// Define the reusable scan history card widget
-Widget _buildScanHistoryCard(
-  BuildContext context, {
-  required String imagePath,
-  required String deteksi,
-  required String tanggal,
-  required String saran,
-  required bool isSelected,
-}) {
+  // Widget untuk setiap kartu penyakit
+  Widget _buildDiseaseCard(String name, String imagePath) {
   return GestureDetector(
     onTap: () {
-      // Navigate to the ScanResultScreen and pass the imagePath
-      Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (context) => ScanResultScreen(imagePath: imagePath),
-        ),
-      );
+       // Menambahkan logika if-else untuk menangani masing-masing penyakit
+      if (name == 'Canker') {
+        
+      } else if (name == 'Greening') {
+       
+      } else if (name == 'Black Spot') {
+      } else if (name == 'Melanose') {
+      }
     },
-    child: Container(
-      decoration: BoxDecoration(
-        color: Color(0xFF215C3C).withOpacity(0.1),
+    child: Card(
+      shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(12),
       ),
-      padding: EdgeInsets.all(16),
-      margin: EdgeInsets.only(bottom: 16),
+      elevation: 4,
       child: Stack(
         children: [
-          Row(
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(12),
-                child: Image.asset(
-                  imagePath,
-                  width: 80,
-                  height: 80,
-                  fit: BoxFit.cover,
-                ),
-              ),
-              SizedBox(width: 16),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      deteksi,
-                      style:
-                          TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(height: 4),
-                    Text(
-                      saran,
-                      style: TextStyle(fontSize: 14, color: Colors.black54),
-                    ),
-                    SizedBox(height: 4),
-                    Text(
-                      tanggal,
-                      style: TextStyle(fontSize: 15, color: Colors.black54),
-                    ),
+          // Gambar latar belakang
+          ClipRRect(
+            borderRadius: BorderRadius.circular(12),
+            child: Image.asset(
+              imagePath,
+              fit: BoxFit.cover,
+              width: double.infinity,
+              height: double.infinity,
+            ),
+          ),
+          // Layer dengan efek gradasi hitam
+          ClipRRect(
+            borderRadius: BorderRadius.circular(12),
+            child: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Color(0xFF215C3C).withOpacity(0.5), // Bagian atas lebih terang
+                    Color(0xFF215C3C).withOpacity(0.7), // Bagian bawah lebih gelap
                   ],
                 ),
               ),
-            ],
+            ),
+          ),
+          // Teks di atas gambar
+          Positioned(
+            bottom: 8, // Jarak dari bagian bawah
+            left: 8,   // Jarak dari bagian kiri
+            right: 8,  // Jarak dari bagian kanan
+            child: Text(
+              name,
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: Colors.white, // Warna teks putih
+                shadows: [
+                  Shadow(
+                    offset: Offset(0, 1), // Bayangan vertikal
+                    blurRadius: 2,
+                    color: Color(0xFF215C3C).withOpacity(0.5),
+                  ),
+                ],
+              ),
+            ),
           ),
         ],
       ),
     ),
   );
+}
+
 }
